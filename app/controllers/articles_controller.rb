3daddf5a75d163def8a1a3a3db7454e8a21a4061
Yukito Shibuya <shibuya@dotinstall.com>
2015-03-09 15:15:36 +0900

    Developed show of article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,14 @@
 class ArticlesController < ApplicationController
+  before_action :set_article,only:[:show]
+  def show
+  end
+
   def new
     @article = Article.new
+  end 
+
+  private 
+  def set_article
+    @article = article.find(id: params[:id])
   end 
 end
