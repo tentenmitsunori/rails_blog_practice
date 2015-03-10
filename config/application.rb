c66ebd53c29369d2a7dde94226b0c6e78885e27c
Yukito Shibuya <shibuya@dotinstall.com>
2015-03-10 15:40:23 +0900

    Change message Englist to Japanese

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module RailsBlogPractice
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -18,7 +19,7 @@ module RailsBlogPractice
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
