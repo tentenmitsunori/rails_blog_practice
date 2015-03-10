88ea0b38980b12a00e5f8112e46aa7ceef7d6917
Yukito Shibuya <shibuya@dotinstall.com>
2015-03-10 17:39:29 +0900

    Separated ja.yml under model directory

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module RailsBlogPractice
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -18,7 +17,7 @@ module RailsBlogPractice
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
